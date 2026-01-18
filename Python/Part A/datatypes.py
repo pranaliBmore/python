6bd65290b2c526452feeaa1c88dcce90dd19f3cf
pranaliBmore <morepranali0007@gmail.com>
2026-01-19 00:15:58 +0530

    Added datatypes and their info

---
+++ Python/Part A/datatypes.py
@@ -0,0 +1,23 @@
+# Fundamental Data Types 
+
+int 
+float 
+bool
+str
+list 
+tuple
+set
+dict
+
+# classes -> custom types
+
+# Specialized Data Types
+
+# Numbers
+# int 
+# float
+
+print( 2 + 4 )
+print( 2 - 4 )
+print( 2 / 4 )
+print( 2 * 4 )
