927638a17561df53b20bff6a0e49de50cbec903b
pranaliBmore <morepranali0007@gmail.com>
2026-01-19 18:16:41 +0530

    use of type keword

--- Python/Part A/datatypes.py
+++ Python/Part A/datatypes.py
@@ -21,3 +21,11 @@ print( 2 + 4 )
 print( 2 - 4 )
 print( 2 / 4 )
 print( 2 * 4 )
+
+
+# Checking the type of the result
+
+print(type(2+4))
+print(type(2-4))
+print(type(2*4))
+print(type(2/4))  # 0.5 is float
