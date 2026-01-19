fd2a3975a074607abfa04d7a726ad000288202e6
pranaliBmore <morepranali0007@gmail.com>
2026-01-19 18:31:40 +0530

    information_about_other_arithmetic_operators

--- Python/Part A/datatypes.py
+++ Python/Part A/datatypes.py
@@ -25,7 +25,12 @@ print( 2 * 4 )
 
 # Checking the type of the result
 
-print(type(2+4))
-print(type(2-4))
-print(type(2*4))
-print(type(2/4))  # 0.5 is float
+print(type(2 + 4))
+print(type(2 - 4))
+print(type(2 * 4))
+print(type(2 / 4))  # 0.5 is float
+print(type(9 + 1.9)) # int + float = float
+
+print(2 ** 3) # exponentiation
+print(5 // 4) # floor division
+print(6 % 4) # modulus
