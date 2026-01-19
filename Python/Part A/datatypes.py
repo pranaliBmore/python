23443c6d24018bcfdf2bf4cd2070200faf363add
pranaliBmore <morepranali0007@gmail.com>
2026-01-19 18:41:11 +0530

    Removed unwanted folder

--- Python/Part A/datatypes.py
+++ Python/Part A/datatypes.py
@@ -31,6 +31,7 @@ print(type(2 * 4))
 print(type(2 / 4))  # 0.5 is float
 print(type(9 + 1.9)) # int + float = float
 
+
 print(2 ** 3) # exponentiation
 print(5 // 4) # floor division
 print(6 % 4) # modulus
