07015bfbd4af18494edc6b17b5f1f3043a248c4c
pranaliBmore <morepranali0007@gmail.com>
2026-01-20 07:16:18 +0530

    Added Code

--- part_a/variables.py
+++ part_a/variables.py
@@ -1,7 +1,15 @@
+iq = 150
+
+print(iq)
+
 user_name = "Pranali"
 
 print("Hello, " + user_name + "! Welcome to Python programming.")   
 
 age = 20
 print("You are " + str(age) + " years old.")
+
+
+# Constant variable
+PI = 3.14
 
