9d5cccf4e871434fff56c9b260b23b0c53c97881
pranaliBmore <morepranali0007@gmail.com>
2026-01-20 07:24:09 +0530

    Added Code

--- part_a/agumented_assignment_operator.py
+++ part_a/agumented_assignment_operator.py
@@ -1,3 +1,22 @@
 # Augmented Assignment Operators
 # Augmented assignment operators are a shorthand way to perform operations on variables and update their values in place.
 
+x = 5
+x += 3  # Equivalent to x = x + 3
+print(x)  # Output: 8
+
+y = 10
+y *= 2  # Equivalent to y = y * 2
+print(y)  # Output: 20
+
+z = 15
+z -= 5  # Equivalent to z = z - 5
+print(z)  # Output: 10 
+
+a = 20
+a /= 4 # Equivalent to a = a / 4
+print(a) # Output: 5.0
+
+b = 7
+b %= 3 # Equivalent to b = b % 3
+print(b) # Output: 1
