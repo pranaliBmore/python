c05e6f2720e90935d3c04de8c7ccea12b6bd1dca
pranaliBmore <morepranali0007@gmail.com>
2026-01-21 17:54:23 +0530

    Added Code in escape sequence

---
+++ part_a/escape_sequence.py
@@ -0,0 +1,16 @@
+# Escape sequences in Python
+
+# Using backslash (\) to include special characters in strings
+
+weather = "It\'s \"kind of\" sunny"
+weather1 = 'It\\s "kind of" sunny'
+
+# Using tab (\t) and newline (\n) escape sequences
+weather2 = "\t It\'s \"kind of\" sunny"
+weather3 = "\t It's \"kind of\" sunny \n Hope you have a good day!"
+
+# Printing the strings to see the effect of escape sequences
+print(weather) 
+print(weather1)
+print(weather2)
+print(weather3)
