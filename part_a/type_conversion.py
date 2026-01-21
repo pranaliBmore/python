eef2085ce9c131c929f3f74d4c474e1360d96149
pranaliBmore <morepranali0007@gmail.com>
2026-01-21 17:41:09 +0530

    Type Conversion Examples

---
+++ part_a/type_conversion.py
@@ -0,0 +1,9 @@
+# This code demonstrates type conversion in Python.
+# In type conversion we convert one data type to another data type.
+
+print(type(str(100))) 
+
+a = str(100)
+b = int(a)
+c = type(b)
+print(c) 
