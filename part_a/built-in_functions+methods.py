afc63799de22206b19060c64ed0c9f3dc2713a15
pranaliBmore <morepranali0007@gmail.com>
2026-02-28 10:36:40 +0530

    Updated Code

--- part_a/built-in_functions+methods.py
+++ part_a/built-in_functions+methods.py
@@ -4,4 +4,21 @@
 int()
 print()'''
  
-print(len('Hellooooo'))
+greet = 'Hellooooo'
+print(greet[:])
+# Output: Hellooooo
+
+print(greet[0:len(greet)])
+# Output: Hellooooo
+
+# functions are reusable pieces of code that perform a specific task. They can take input, process it, and return an output.
+# Methods are functions that are associated with a specific data type or object. They are called on an instance of the object and can modify the object or return a value based on the object's state.
+
+quote = "to be or not to be"
+
+print(quote.upper())
+# Output: TO BE OR NOT TO BE
+
+print(quote.capitalize())
+# Output : To be or not be
+
