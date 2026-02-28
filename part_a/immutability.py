774c39262c9aa71eb15d56f00f64132c06eca4d4
pranaliBmore <morepranali0007@gmail.com>
2026-02-28 10:17:39 +0530

    Initial Commit

---
+++ part_a/immutability.py
@@ -0,0 +1,6 @@
+''' 
+Immutabililty is a property of an object whose state cannot be modified after it is created. 
+In Python, immutability is often associated with certain data types, such as strings, tuples, and frozensets. 
+These types cannot be changed after they are created, which can help prevent unintended side effects and make code easier to reason about.
+
+'''
